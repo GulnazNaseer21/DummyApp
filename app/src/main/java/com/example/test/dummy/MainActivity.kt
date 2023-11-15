a2e81cc91d9857400cc947bfba50294a2ad6acc9
Desk9 <desk9@hypersoftinc.com>
2023-11-15 01:44:10 -0800

    11-15-23

--- app/src/main/java/com/example/test/dummy/MainActivity.kt
+++ app/src/main/java/com/example/test/dummy/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.test.dummy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val toast = Toast.makeText(
+            applicationContext,
+            "Your string here", Toast.LENGTH_SHORT
+        )
+        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 0)
+        toast.show()
     }
+
+
 }
